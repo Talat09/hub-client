8377805d5d90c27c1882dd9e107aab18ac5bd751
Talat09 <talat181008312@gmail.com>
2023-12-22 03:21:08 +0600

    done show best seller data and show top rated data

--- src/components/BestSeller/BestSeller.js
+++ src/components/BestSeller/BestSeller.js
@@ -1,7 +1,31 @@
 import React from "react";
+import axiosPrivate from "../../api/axiosPrivate";
+import { useQuery } from "react-query";
+import LoadingSpinner from "../Shared/LoadingSpinner/LoadingSpinner";
+import PartsCard from "../PartsCard/PartsCard";
 
 const BestSeller = () => {
-  return <div>Best Seller</div>;
+  const getParts = async () => {
+    const { data } = await axiosPrivate.get(
+      `http://localhost:5000/parts?page=0&size=10}`
+    );
+    return data;
+  };
+  const { data, isLoading } = useQuery(["three-parts"], getParts, {
+    keepPreviousData: true,
+  });
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <div className="grid lg:grid-cols-3 gap-7">
+      {data?.data?.slice(3, 6).map((item) => (
+        <PartsCard parts={item} />
+      ))}
+    </div>
+  );
 };
 
 export default BestSeller;
--- src/components/TopRated/TopRated.js
+++ src/components/TopRated/TopRated.js
@@ -1,7 +1,31 @@
 import React from "react";
+import axiosPrivate from "../../api/axiosPrivate";
+import { useQuery } from "react-query";
+import LoadingSpinner from "../Shared/LoadingSpinner/LoadingSpinner";
+import PartsCard from "../PartsCard/PartsCard";
 
 const TopRated = () => {
-  return <div>Top Rated</div>;
+  const getParts = async () => {
+    const { data } = await axiosPrivate.get(
+      `https://parts-bd-inc-server.vercel.app/parts?page=0&size=10}`
+    );
+    return data;
+  };
+  const { data, isLoading } = useQuery(["three-parts"], getParts, {
+    keepPreviousData: true,
+  });
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <div className="grid lg:grid-cols-3 gap-7">
+      {data?.data?.slice(0, 3).map((item) => (
+        <PartsCard parts={item} />
+      ))}
+    </div>
+  );
 };
 
 export default TopRated;
